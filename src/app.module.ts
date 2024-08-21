39966ce8e8116d5993f4b5483aa3b4711432c42d
Kim-Hyunhee <tta_bi@naver.com>
2024-08-21 18:54:22 +0900

    Chore: 디렉토리 구조

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeedModule } from './modules/feed/feed.module';
+import { UserModule } from './modules/user/user.module';
+import { VerificationModule } from './modules/verification/verification.module';
+
 
 @Module({
-  imports: [],
+  imports: [FeedModule, UserModule, VerificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
