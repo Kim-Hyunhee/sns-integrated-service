215704030137a5ef51e613be4b322730f7f519b8
urimJ <ufrl128@dgu.ac.kr>
2024-08-23 05:26:22 +0900

    Feat: 사용자 회원가입 API 구현 #11

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Register } from '@app/auth/dto/auth.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/register')
+  @ApiResponse({ status: 200, description: '회원 가입되었습니다' })
+  @ApiResponse({
+    status: 400,
+    description: '이미 사용 중인 계정/비밀번호 조건 위반/잘못된 이메일 구조',
+  })
+  @UsePipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const errorMessages = errors.map((error) => {
+          if (error.property === 'email') {
+            return '이메일 구조가 올바르지 않습니다.';
+          }
+          return Object.values(error.constraints).join(', ');
+        });
+        return new BadRequestException({
+          success: false,
+          statusCode: 400,
+          error: errorMessages,
+        });
+      },
+    }),
+  )
+  async register(@Body() register: Register) {
+    return this.userService.register(register);
+  }
+}
+
+//@Post('/login')
