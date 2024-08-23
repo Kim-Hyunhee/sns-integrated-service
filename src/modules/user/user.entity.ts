7d1e2933f60266a4546d62e07711a09880186364
raminicano <raminicayes@gmail.com>
2024-08-23 10:15:28 +0900

    Refactor: 유저아이디 수정 - int에서 uuid로 변경

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -11,7 +11,7 @@ import { uuidv7 } from 'uuidv7';
 import { Buffer } from 'buffer';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import * as bcrypt from 'bcrypt'; // 비밀번호 암호화
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -66,6 +66,25 @@ export class User {
 
   // 비밀번호 암호화
   @BeforeInsert()
+  async generateId() {
+    let unique = false;
+
+    while (!unique) {
+      const uuidString = uuidv7();
+      const uuidBuffer = Buffer.from(uuidString.replace(/-/g, ''), 'hex');
+
+      const existingUser = await this.userRepository.findOne({
+        where: { userId: uuidBuffer },
+      });
+
+      if (!existingUser) {
+        this.userId = uuidBuffer;
+        unique = true;
+      }
+    }
+  }
+
+  // 비밀번호 암호화
   private async hashPassword() {
     if (this.password) {
       this.password = await bcrypt.hash(this.password, 10);
